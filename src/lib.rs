66f3bfde7d680920c9bc78ba44ddb12d94723745
liguangliang <13537223157@163.com>
2024-08-16 11:06:48 +0800

    feat: add iterator

--- src/lib.rs
+++ src/lib.rs
@@ -21,12 +21,16 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            panic!("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let file_path = match args.next() {
+            Some(arg)=> arg,
+            None => return Err("Didn't get a file path"),
+        };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
         Ok(Config {
             query,
@@ -36,13 +40,7 @@ impl Config {
     }
 }
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for content in contents.lines() {
-        if content.contains(&query) {
-            results.push(content);
-        }
-    }
-    results
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use std::{env, process};
 use minigrep::Config;
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
     println!("Searching for {} in {}", config.query, config.file_path);
     if let Err(err) = minigrep::run(config){
-        println!("Application error: {}", err);
+        eprintln!("Application error: {}", err);
         process::exit(1);
     }
 }
